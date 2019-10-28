df966444095306228825c943f8e584aa248708a0
carlos.allendesanz <carlos.allendesanz@farmersinsurance.com>
2019-10-28 13:00:24 -0700

    adding data

--- policies.js
+++ policies.js
@@ -4,3 +4,110 @@ function formSubmission() {
   var model = document.getElementById("model").value;
   console.log(year, make, model);
 };
+
+
+var quoteResponses = [
+  {
+    type: 'electric',
+    price: '$500 / 6 months',
+    effectiveDate: 'January 1, 2020',
+    expirationDaye: 'July 1 2020',
+    coverages: ['Collision', 'Personal Injury', 'Uninsured Motorist', 'Comprenhensive', 'Rental', 'Road Assistance'],
+    comparassion: [
+      {
+        car: '2019 Tesla Model X',
+        price: '$600'
+      },
+      {
+        car: '2019 Tesla Model S',
+        price: '$700'
+      },
+      {
+        car: '2019 Chevy Bolt',
+        price: '$350'
+      },
+      {
+        car: '2019 Nisan Leaf',
+        price: '$350'
+      }
+    ]
+
+},
+  {
+    type: 'muscle',
+    price: '$600 / 6 months',
+    effectiveDate: 'February 2, 2020',
+    expirationDaye: 'August 2 2020',
+    coverages: ['Comprenhensive', 'Rental', 'Collision', 'Personal Injury'],
+    comparassion: [
+      {
+        car: '2018 Ford Mustang',
+        price: '$600'
+      },
+      {
+        car: '2018 Chevrolet Camaro',
+        price: '$700'
+      },
+      {
+        car: '2018 Dodge Challenger',
+        price: '$600'
+      },
+      {
+        car: '2018 Lunar Rover',
+        price: '$750'
+      }
+    ]
+
+},
+  {
+    type: 'pickup',
+    price: '$440 / 6 months',
+    effectiveDate: 'March 3, 2020',
+    expirationDaye: 'September 3 2020',
+    coverages: ['Collision', 'Personal Injury', 'Uninsured Motorist', 'Comprenhensive', 'Rental', 'Road Assistance'],
+    comparassion: [
+      {
+        car: '2018 Toyota Takoma',
+        price: '$700'
+      },
+      {
+        car: '2018 Ford F150',
+        price: '$800'
+      },
+      {
+        car: '2018 Ford Rapter',
+        price: '$850'
+      },
+      {
+        car: '2018 Toyota Tundra',
+        price: '$'
+      }
+    ]
+
+},
+  {
+    type: 'SUV',
+    price: '$750 / 6 months',
+    effectiveDate: 'April 4, 2020',
+    expirationDaye: 'October 4 2020',
+    coverages: ['Collision', 'Personal Injury', 'Uninsured Motorist', 'Road Assistance'],
+    comparassion: [
+      {
+        car: '2019 Toyota RAV4',
+        price: '$1000'
+      },
+      {
+        car: '2019 Ranger Rover Sport',
+        price: '$900'
+      },
+      {
+        car: '2019 Acura MDX',
+        price: '$800'
+      },
+      {
+        car: '2019 Ford Scape',
+        price: '$750'
+      }
+    ]
+  }
+];
