8c5e311b12ef9d720eb8c5e72b598f6e2131832d
carlos.allendesanz <carlos.allendesanz@farmersinsurance.com>
2019-10-28 12:37:33 -0700

    form added

--- policies.js
+++ policies.js
@@ -1,38 +1,6 @@
-function toCelsius(fahrenheit) {
-  return (5 / 9) * (fahrenheit - 32);
-}
-function toFarenheit(Celsius) {
-  return (Celsius * 9) / 5 + 32;
-}
-
-
-function myFunction() {
-  var check = document.getElementById("myNumber").value;
-  var FarCheck = document.getElementById("Fahrenheit").checked;
-  var CelCheck = document.getElementById("Celsius").checked;
-
-  if (check === "" || (FarCheck == false && CelCheck == false)) {
-    document.getElementById("demo").innerHTML = "Please fill out everything";
-  } else {
-    if (document.getElementById("Celsius").checked) {
-      var temp = document.getElementById("myNumber").value;
-      console.log(temp);
-      if (isNaN(temp)) {
-        document.getElementById("demo").innerHTML =
-          "Please enter a number not a letter";
-      } else {
-        document.getElementById("demo").innerHTML =
-          temp + " C = " + toFarenheit(temp) + " F ";
-      }
-    } else if (document.getElementById("Fahrenheit").checked) {
-      var temp1 = document.getElementById("myNumber").value;
-      if (isNaN(temp1)) {
-        document.getElementById("demo").innerHTML =
-          "Please enter a number not a letter";
-      } else {
-        document.getElementById("demo").innerHTML =
-          temp1 + " F = " + toCelsius(temp1).toFixed(2) + " C ";
-      }
-    }
-  }
-}
+function formSubmission() {
+  var year = document.getElementById("year").value;
+  var make = document.getElementById("make").value;
+  var model = document.getElementById("model").value;
+  console.log(year, make, model);
+};
